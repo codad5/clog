56550e1b96394a7e6bc8fd61f85fd5bb228f9248
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-09-17 13:50:23 +0100

    Nav fix 2

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -1,10 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">{{ config('app.name', 'CLOG') }}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">{{ config('app.name', 'CLOG') }}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExample02">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" aria-current="page" href="/">Home</a>
@@ -17,7 +18,7 @@
           </li>
           @if(Auth::check())
         <li class="nav-item dropdown">
-            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+            <button class="nav-link btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 Admins Actions
             </button>
