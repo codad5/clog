b255da61774390ef8519b27bc7f8e4bf33891ab2
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-09-17 13:06:07 +0100

    Fixes

---
+++ app/Http/Controllers/UsersAuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+// use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\Post;
+
+class UsersAuthController extends Controller
+{
+    //
+    public function index(){
+        if($this->isLoggedIn()){
+            return redirect('/dashboard');
+        }
+        return view('auth.login');
+    }
+
+    public function login(Request $request){
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard')->with('success', 'You are now logged in');
+        }
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+
+    
+
+
+    public static function isLoggedIn(){
+        if(Auth::check()){
+            return true;
+        }
+        return false;
+    }
+    public function registration(){
+        if($this->isLoggedIn()){
+            return redirect('/posts');
+        }
+        return view('auth.registration');
+    }
+
+    public function postRegistration(Request $request){
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    public function dashboard(){
+            // $post = Post::find();
+            $user_id = auth()->user('id');
+            $user = User::find($user_id->id);
+            // echo '<pre>';
+            // print_r($user->posts);
+            // echo '</pre>';
+        
+            return view('auth.dashboard')->with('posts', $user->posts);
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+        return back();
+    }
+}
